c0b86dda6571044a9493b3b6969101d6406621ff
Huy Bui <nhathuy.dev@gmail.com>
2018-01-03 11:41:37 +0700

    merge

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,12 @@
 <?php
 
 use Illuminate\Http\Request;
-Route::domain('api.45.32.127.139')->namespace('Api')->group(function () {
+//Route::domain('api.45.32.127.139')->namespace('Api')->group(function () {
     Route::get('/test', 'TestController');
     Route::resource('answer', 'AnswerController');
     Route::resource('question', 'QuestionController');
     Route::resource('tag', 'TagController');
-});
+//});
 /*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });*/
