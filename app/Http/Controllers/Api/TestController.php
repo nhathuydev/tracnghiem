f98f2a86487200d203c662c3fa09dfd8f067afc2
Huy Bui <nhathuy.dev@gmail.com>
2018-04-02 10:23:13 +0700

    merge

--- app/Http/Controllers/Api/TestController.php
+++ app/Http/Controllers/Api/TestController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Events\TestEvent;
 use App\Jobs\TestJob;
 use App\Repository\Question\QuestionRepository;
 use Illuminate\Http\Request;
@@ -19,10 +20,11 @@ class TestController extends Controller
     }
     public function test(Request $request)
     {
+        event(new TestEvent(123));
 //        $result = $this->question->get(3)->answers;
 //        $result = $this->question->get(3);
-        Log::info('Dispatch job start at: ' . now()->timestamp);
-        dd(TestJob::dispatch(['meme' => 1])->delay(now()->addSeconds(10)));
+//        Log::info('Dispatch job start at: ' . now()->timestamp);
+//        dd(TestJob::dispatch(['meme' => 1])->delay(now()->addSeconds(10)));
         return response()->success(auth()->guard('api')->user());
     }
 
---
+++ bootstrap/constants.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mypop
+ * Date: 3/30/18
+ * Time: 9:08 AM
+ */
+
+define('ANSWER_SHEET_WORKING', 0);
+define('ANSWER_SHEET_DONE', 1);
+define('ANSWER_SHEET_CANCEL', 2);
